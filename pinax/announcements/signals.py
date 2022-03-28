35d20789e9ddc8f0565ef2a512cfafd957af9fab
Craig de Stigter <craig@destigter.nz>
2022-03-29 10:17:16 +1300

    Add Django 4.0 support

--- pinax/announcements/signals.py
+++ pinax/announcements/signals.py
@@ -1,5 +1,8 @@
 import django.dispatch
 
-announcement_created = django.dispatch.Signal(providing_args=["announcement", "request"])
-announcement_updated = django.dispatch.Signal(providing_args=["announcement", "request"])
-announcement_deleted = django.dispatch.Signal(providing_args=["announcement", "request"])
+# provides args: announcement, request
+announcement_created = django.dispatch.Signal()
+# provides args: announcement, request
+announcement_updated = django.dispatch.Signal()
+# provides args: announcement, request
+announcement_deleted = django.dispatch.Signal()
